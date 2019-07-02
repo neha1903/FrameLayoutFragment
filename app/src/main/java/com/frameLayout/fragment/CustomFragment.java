152588bceec0218fb532c21701fd8e4a3f2b7b4d
neha1903 <nehamakkar1903@gmail.com>
2019-07-02 23:58:53 +0530

    commit

---
+++ app/src/main/java/com/frameLayout/fragment/CustomFragment.java
@@ -0,0 +1,18 @@
+package com.frameLayout.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CustomFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** Inflating the layout for this fragment **/
+        View v = inflater.inflate(R.layout.custom_fragment_layout, null);
+
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/frameLayout/fragment/MainActivity.java
@@ -0,0 +1,33 @@
+package com.frameLayout.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLoad = (Button) findViewById(R.id.btn_load);
+        btnLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                CustomFragment hello = new CustomFragment();
+                fragmentTransaction.add(R.id.fragment_container, hello, "HELLO");
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
+    }
+}
